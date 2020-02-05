b8881a158b389da99747a1eac0af73c083086f32
Sergey Kolodyazhnyy <kolodyaz@adobe.com>
2020-02-05 17:12:55 +0100

    Open sourcify

--- pkg/keychainx/keychain_darwin.go
+++ pkg/keychainx/keychain_darwin.go
@@ -1,5 +1,16 @@
 // +build darwin
 
+/*
+Copyright 2019 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
+*/
+
 package keychainx
 
 import (
